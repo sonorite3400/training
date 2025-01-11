25f2fdfec73b71b97571be2ef379007d01594165
sonorite3400 <129268110+sonorite3400@users.noreply.github.com>
2025-01-12 01:11:28 +0900

    エラーハンドリング

--- echo-server/client.c
+++ echo-server/client.c
@@ -8,25 +8,77 @@
 // TODO: Bufferサイズは何を基準にする？
 #define BUFFER_SIZE 1024
 
+int EchoRequest(int socketFd)
+{
+    char sendBuffer[BUFFER_SIZE], recvBuffer[BUFFER_SIZE];
+    int sendSize, recvSize;
+
+    while (1)
+    {
+        fgets(sendBuffer, BUFFER_SIZE, stdin);
+        // TODO: 一度にすべて送れなかった場合の対応
+        sendSize = send(socketFd, sendBuffer, BUFFER_SIZE, 0);
+        if (sendSize == -1)
+        {
+            perror("Error: send() failed\n");
+            if (close(socketFd) == -1)
+            {
+                perror("Error: close() failed\n");
+                return 1;
+            }
+            return 1;
+        }
+        recvSize = recv(socketFd, recvBuffer, BUFFER_SIZE, 0);
+        if (recvSize == -1)
+        {
+            perror("Error: recv() failed");
+            if (close(socketFd) == -1)
+            {
+                perror("Error: close() failed\n");
+                return 1;
+            }
+            return 1;
+        }
+        if (recvSize == 0)
+        {
+            puts("server did not respond\n");
+            break;
+        }
+        puts(recvBuffer);
+    }
+    if (close(socketFd) == -1)
+    {
+        perror("Error: close() failed\n");
+        return 1;
+    }
+    return 0;
+}
+
 int main()
 {
     struct sockaddr_in serverAddr;
-    char sendBuffer[BUFFER_SIZE], recvBuffer[BUFFER_SIZE];
 
     int socketFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFd == -1)
+    {
+        perror("Error: socket() failed\n");
+        return 1;
+    }
 
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serverAddr.sin_port = htons(8080);
-    connect(socketFd, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    if (connect(socketFd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
+    {
+        perror("Error: connect() failed\n");
+        return 1;
+    }
 
     // echo
-    fgets(sendBuffer, BUFFER_SIZE, stdin);
-    send(socketFd, sendBuffer, BUFFER_SIZE, 0);
-    recv(socketFd, recvBuffer, BUFFER_SIZE, 0);
-    puts(recvBuffer);
-
-    close(socketFd);
+    if (EchoRequest(socketFd) == 1)
+    {
+        return 1;
+    }
     return 0;
 }
--- echo-server/server.c
+++ echo-server/server.c
@@ -7,31 +7,102 @@
 // TODO: Bufferサイズは何を基準にする？
 #define BUFFER_SIZE 1024
 
+int Echo(int clientFd)
+{
+    char recvBuffer[BUFFER_SIZE];
+
+    while (1)
+    {
+        int recvSize = recv(clientFd, recvBuffer, BUFFER_SIZE, 0);
+        if (recvSize == -1)
+        {
+            perror("Error: recv() failed\n");
+            if (close(clientFd) == -1)
+            {
+                perror("Error: close() failed\n");
+                return 1;
+            }
+            break;
+        }
+        else if (recvSize == 0)
+        {
+            if (close(clientFd) == -1)
+            {
+                perror("Error: close() failed\n");
+                return 1;
+            }
+            break;
+        }
+        // TODO: 一度にすべて送れなかった場合の対応
+        if (send(clientFd, recvBuffer, recvSize, 0) == -1)
+        {
+            perror("Error: send() failed\n");
+            if (close(clientFd) == -1)
+            {
+                perror("Error: close() failed\n");
+                return 1;
+            }
+            break;
+        }
+    }
+    return 0;
+}
+
 int main()
 {
     struct sockaddr_in serverAddr, clientAddr;
-    char recvBuffer[BUFFER_SIZE];
 
-    // AF_INET: IP protocol family
-    // SOCK_STREAM: Sequenced, reliable, connection-based byte streams
     int socketFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFd == -1)
+    {
+        perror("Error: socket() failed\n");
+        return 1;
+    }
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
-    // INADDR_ANY: Address to accept any incoming messages
     serverAddr.sin_addr.s_addr = INADDR_ANY;
     serverAddr.sin_port = htons(8080);
-    bind(socketFd, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    if (bind(socketFd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
+    {
+        perror("Error: bind() failed\n");
+        if (close(socketFd) == -1)
+        {
+            perror("Error: close() failed\n");
+            return 1;
+        }
+        return 1;
+    }
 
     // TODO: backlogに入れるべき大きさは何を基準にする？
-    listen(socketFd, 5);
+    if (listen(socketFd, 5) == -1)
+    {
+        perror("Error: listen() failed\n");
+        if (close(socketFd) == -1)
+        {
+            perror("Error: close() failed\n");
+            return 1;
+        }
+        return 1;
+    };
 
-    unsigned int clientAddrLength = sizeof(clientAddr);
-    int acceptSocketFd = accept(socketFd, (struct sockaddr *)&clientAddr, &clientAddrLength);
-
-    // echo
-    int recvSize = recv(acceptSocketFd, recvBuffer, BUFFER_SIZE, 0);
-    send(acceptSocketFd, recvBuffer, recvSize, 0);
-
-    close(socketFd);
-    return 0;
+    // TODO: マルチスレッドにする
+    while (1)
+    {
+        unsigned int clientAddrLength = sizeof(clientAddr);
+        int acceptedSocketFd = accept(socketFd, (struct sockaddr *)&clientAddr, &clientAddrLength);
+        if (acceptedSocketFd == -1)
+        {
+            perror("Error: accept() failed\n");
+            if (close(socketFd) == -1)
+            {
+                perror("Error: close() failed\n");
+                return 1;
+            }
+            continue;
+        }
+        if (Echo(acceptedSocketFd) == 1)
+        {
+            return 1;
+        }
+    }
 }
